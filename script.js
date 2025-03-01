49c806a6cd08c67c780ca67ca02e825df94eca84
Adrian <adrianbyers@gmail.com>
2025-03-01 18:05:11 -0500

    Add displayLib function and button for testing

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const myLibrary = [];
+const myLibrary = [Book1, Book2];
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -10,10 +10,17 @@ function Book(title, author, pages, read) {
       `${this.title} by ${this.author}, ${this.pages} pages, ${this.read}`
     )
   }
-  console.log(this.info())
-  theHobbit.info(); // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
 }
 
-function addBook() {
+function addBookToLibrary(title, author, pages, read) {
   // take params, create a book then store it in the array
+const newBook = new Book(title, author, pages, read);
+myLibrary.push(newBook)
 }
+
+function showLib() {
+  myLibrary.forEach(console.log(this.info)) 
+}
+
+let Book1 = new Book("The Hobbit", "JRR Tolkein", 1, read);
+let Book2 = new Book(test, test, test, test);
